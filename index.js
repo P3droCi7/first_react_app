409a0dd6ee0aa740288b6853efe681404d23a1b0
Michal <michalskrzypczak91@gmail.com>
2019-12-03 02:04:12 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//declare const value
+const michal = 'michal';
+
+// define the virable - old way
+var name = 'Michał';
+
+//change variables value
+name = 'Bartek';
+
+//prompt inside the internet browser
+alert(name);
+alert(michal);
